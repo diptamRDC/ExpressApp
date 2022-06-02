4783789e9f4c4f58c4b2f6f04fb8e97c24324988
Diptam halder <halderdiptam@gmail.com>
2022-06-02 21:27:06 +0530

    Static Query Woring and fetching data

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 // create express instance
+
+/**
+ * Url Example : http://localhost:3000/api/query/?instance_url={instance IRL}&access_token={Session Id}&query={query}
+ */
+//
 const express = require("express");
 var app = express();
+var jsforce = require("jsforce");
 
-// set the view engine to ejs
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get("/api/query/", function (req, res) {
+  const jsonQuery = req.query;
+
+  const instance_url = jsonQuery.instance_url;
+  const access_token = jsonQuery.access_token;
+  const query = jsonQuery.query;
+
+  conn = new jsforce.Connection({
+    serverUrl: instance_url,
+    sessionId: access_token,
+  });
+  var records = [];
+  conn.query("SELECT Id, Name FROM Account", function (err, result) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("total : " + result.totalSize);
+    console.log("fetched : " + result.records.length);
+  });
+  console.log("here ===" + conn);
+
+  res.send("Logged in" + conn.identity);
 });
 
 // start express app on port 3000
