f246f1321c498ac3045d204f421fb99123d01c4e
Diptam halder <halderdiptam@gmail.com>
2022-06-02 17:39:56 +0530

    Printing Hello world Code Snapshort

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-console.log("Hello World");
-// create express app
-var express = require("express");
+// create express instance
+const express = require("express");
 var app = express();
 
 // set the view engine to ejs
 app.get("/", function (req, res) {
-  res.render("index");
+  res.send("Hello World!");
 });
 
 // start express app on port 3000
-app.listen(3000); // http://localhost:3000/
-console.log("Express app started on port 3000");
+//app.listen(3000); // http://localhost:3000/
 
-// var server = app.listen(3000, function () {
-// //   var host = server.address().address;
-// //   var port = server.address().port;
-
-// //   console.log("Example app listening at http://%s:%s", host, port);
-// });
+app.listen(3000, () => console.log("Express app started on port 3000"));
